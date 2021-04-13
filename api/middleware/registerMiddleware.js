3035d8d4f8edf4cdcd2a61f4504c83e746f0b0b4
NielsMoens <nielmoen@student.arteveldehs.be>
2021-04-14 01:22:26 +0200

    s

--- api/middleware/registerMiddleware.js
+++ api/middleware/registerMiddleware.js
@@ -6,6 +6,11 @@ const registerMiddleware = (app) => {
     // allow all cors
     app.use(cors())
 
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        next();
+      });
+
     // use json
     app.use(express.json());
     app.use(express.urlencoded({
